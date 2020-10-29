243a7fb1b151e2b0b5678382672c1e88106df4ce
LOSKIw <48372803+LOSKIw@users.noreply.github.com>
2020-10-29 13:41:04 +0800

    screeps补全

--- src/roomPlanner.js
+++ src/roomPlanner.js
@@ -1,10 +1,22 @@
 const extensionGNum = 6
 const extensionGroup = [[1,0],[-1,0],[0,1],[0,-1]]
+/*
+config存储配置，如建筑快构造，数目，建造等级
+*/
+let roomConfig = {
+    extensionGroup:{
+        num:10,
+        groupLoc:[[0,0],[1,0],[-1,0],[0,1],[0,-1]],
+        buildLv:[2,2,3,3,4,4,5,5,6,6,7,7,8,8]
+    },
 
-
+}
 class roomPlan{
+    constructor(){
 
+    }   
     getPlan(){
+        
         let layout = {
             spawn: [],
             extension: [],
